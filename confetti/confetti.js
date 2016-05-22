a48c976c2dba2607a054774bde5f8894377da0ba
Wills Bithrey <wills@willsbithrey.com>
2016-05-22 20:16:00 +0100

    Confetti Tweaks

--- confetti/confetti.js
+++ confetti/confetti.js
@@ -28,92 +28,88 @@ const stage = new PIXI.Container();
 // create the Particle Container and add it to the stage.
 const particleContainers = {};
 
-SPRITE_COLOURS.forEach(function (colour) {
-  for (var i = 1; i <= SPRITE_NUM_VARIATIONS_PER_COLOUR; i++) {
-    var totalParticles = Math.ceil(TOTAL_PARTICLES / SPRITE_COLOURS.length / 
-        SPRITE_NUM_VARIATIONS_PER_COLOUR);
-
-    var container = new PIXI.ParticleContainer(totalParticles, {
-      scale: true,
-      position: true,
-      rotation: true,
-      uvs: true,
-      alpha: true
-    });
-
-    particleContainers[colour + '-' + i] = container;
-    stage.addChild(container);
-  }
-});
-
 // create an array to store all the sprites.
 const confetti = [];
 
-for (var i = 0; i < TOTAL_PARTICLES; i++) {
-  // pick a random colour and sprite number.
-  var color = getRandArrayItem(SPRITE_COLOURS);
-  var variant = Math.floor(getRandBetween(1, SPRITE_NUM_VARIATIONS_PER_COLOUR));
-  var particleContainer = particleContainers[color + '-' + variant];
-
-  // create a new sprite.
-  var texturePath = `img/confetti/${color}-${variant}@2x.png`;
-  var particle = PIXI.Sprite.fromImage(texturePath);
-
-  // particle.tint = Math.random() * 0xE8D4CD;
-
-  // set the anchor point so the texture is centerd on the sprite.
-  particle.anchor.set(0.5);
-
-  // set initial scale
-  var scaleFactor = getRandBetween(0.3, 0.4) * 0.4;
-  particle.scale.x = scaleFactor;
-  particle.scale.y = scaleFactor;
-
-  // set initial position
-  var startMin = {
-    x: -100,
-    y: -renderer.height
-  };
-
-  var startMax = {
-    x: renderer.width + 100,
-    y: 0 
-  };
-
-  particle.initialX = getRandBetween(startMin.x, startMax.x);
-  particle.initialY = getRandBetween(startMin.y, startMax.y);
-  particle.x = particle.initialX;
-  particle.y = particle.initialY;
-  particle.seed = Math.random();
-  particle.rotationScalar = getRandBetween(-1, 1) * 0.27;
-
-  // particle.tint = Math.random() * 0x808080;
-
-  // create a random direction in radians.
-  particle.direction = Math.PI * 2;
-  // 2 & Math.PI = 360 deg.
-  
-
-  // create a random speed between 0 - 2.
-  particle.speed = (8 + Math.random() * 8) * 0.2;
-
-  particle.offset = Math.random() * 100;
-
-  // finally we push the particle into the particles array so it it can be 
-  // easily accessed later.
-  confetti.push(particle);
-
-  particleContainer.addChild(particle);
-}
-
 // create a bounding box box for the particles.
-var boundsPadding = 50;
-var bounds = new PIXI.Rectangle(-boundsPadding, -boundsPadding, 
-    renderer.width + boundsPadding * 2, renderer.height + boundsPadding * 2);
+const BOUNDS_PADDING = 50;
+var bounds = new PIXI.Rectangle(-BOUNDS_PADDING, -BOUNDS_PADDING, 
+    renderer.width + BOUNDS_PADDING * 2, renderer.height + BOUNDS_PADDING * 2);
 
 var tick = 0;
 
-requestAnimationFrame(animate);
+function init() {
+  SPRITE_COLOURS.forEach(function (colour) {
+    for (var i = 1; i <= SPRITE_NUM_VARIATIONS_PER_COLOUR; i++) {
+      var totalParticles = Math.ceil(TOTAL_PARTICLES / SPRITE_COLOURS.length / 
+          SPRITE_NUM_VARIATIONS_PER_COLOUR);
+
+      var container = new PIXI.ParticleContainer(totalParticles, {
+        scale: true,
+        position: true,
+        rotation: true,
+        uvs: true,
+        alpha: true
+      });
+
+      particleContainers[colour + '-' + i] = container;
+      stage.addChild(container);
+    }
+  });
+
+  for (var i = 0; i < TOTAL_PARTICLES; i++) {
+    // pick a random colour and sprite number.
+    var color = getRandArrayItem(SPRITE_COLOURS);
+    var variant = Math.floor(getRandBetween(1, SPRITE_NUM_VARIATIONS_PER_COLOUR));
+    var particleContainer = particleContainers[color + '-' + variant];
+
+    // create a new sprite.
+    var texturePath = `img/confetti/${color}-${variant}@2x.png`;
+    var particle = PIXI.Sprite.fromImage(texturePath);
+
+    // set the anchor point so the texture is centerd on the sprite.
+    particle.anchor.set(0.5);
+
+    // set initial scale
+    var scaleFactor = getRandBetween(0.3, 0.4) * 0.4;
+    particle.scale.x = scaleFactor;
+    particle.scale.y = scaleFactor;
+
+    // set initial position
+    var startMin = {
+      x: -100,
+      y: -renderer.height
+    };
+
+    var startMax = {
+      x: renderer.width + 100,
+      y: 0 
+    };
+
+    particle.initialX = getRandBetween(startMin.x, startMax.x);
+    particle.initialY = getRandBetween(startMin.y, startMax.y);
+    particle.x = particle.initialX;
+    particle.y = particle.initialY;
+    particle.seed = Math.random();
+    particle.rotationScalar = getRandBetween(-1, 1) * 0.27;
+
+    // create a random direction in radians.
+    particle.direction = Math.PI * 2;
+    
+    // create a random speed between 0 - 2.
+    particle.speed = getRandBetween(3, 10) * 0.3;
+
+    // particle.offset = Math.random() * 100;
+
+    // finally we push the particle into the particles array so it it can be 
+    // easily accessed later.
+    confetti.push(particle);
+
+    particleContainer.addChild(particle);
+  }
+
+  requestAnimationFrame(animate); 
+}
 
 function animate() {
   var windowWidth = window.innerWidth;
@@ -165,6 +161,16 @@ function getRandBetween(min, max) {
   return Math.random() * (max-min+1) + min;
 }
 
-document.addEventListener('mousemove', function (e) {
+function resizeCanvas() {
+  renderer.resize(container.clientWidth, container.clientHeight);
+}
+
+document.addEventListener('mousemove', function(e) {
   mouseX = e.pageX; 
 });
+
+document.addEventListener('resize', function(e) {
+  resizeCanvas();
+});
+
+init();
