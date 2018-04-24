2063279d85f28d67352abb74eaddda013cf18c69
Wills Bithrey <wills@potatolondon.com>
2018-04-24 09:16:28 +0100

    Fixup

--- css-paint/ticket-paint-worklet.js
+++ css-paint/ticket-paint-worklet.js
@@ -1,53 +1,46 @@
-registerPaint('ticket', class {
+registerPaint(
+  "ticket",
+  class {
     static get inputProperties() {
-        return [
-            '--cutout-bottom',
-            '--cutout-radius',
-            '--cutout-top'
-        ];
+      return ["--cutout-radius", "--cutout-edge"];
     }
 
     paint(ctx, geom, properties) {
-        let cutoutTop = properties.get('--cutout-top');
-        let cutoutBottom = properties.get('--cutout-bottom');
-        const radius = parseInt(properties.get('--cutout-radius').toString());
-        const height = geom.height;
-        const width = geom.width;
+      const twoPi = Math.PI * 2;
+      const height = geom.height;
+      const width = geom.width;
+      let radius = properties.get("--cutout-radius");
+      let edge = properties.get("--cutout-edge");
 
-        // console.group('cutout');
-        // console.log('--cutout-radius', radius);
-        // console.log('ctx', ctx);
-        // console.log('properties', properties);
-        // console.log('cutoutTop', cutoutTop, 'cutoutBottom', cutoutBottom);
-        // console.info(height, width);
-        // console.groupEnd();
+      const drawCutout = (x, y) => {
+        ctx.moveTo(x, y);
+        ctx.arc(x, y, radius, 0, twoPi);
+        ctx.closePath();
+        ctx.fill();
+      };
 
-        const radToDeg = (rad) => {
-            return rad * 180 / Math.PI;
-        }
+      // Main ticket area
+      ctx.fillStyle = "#ffffff";
+      ctx.fillRect(0, 0, width, height);
 
-        const drawCutout = (x, y) => {
-            ctx.fillStyle = '#ffffff';
-            ctx.moveTo(x, y);
-            ctx.arc(x, y, radius, radToDeg(0), radToDeg(360));
-            ctx.closePath();
-            ctx.fill();
-        }
+      if (!edge || !radius) {
+        return;
+      }
 
-        // Main ticket area
-        ctx.fillStyle = '#ffffff';
-        ctx.fillRect(0, 0, width, height);
+      radius = parseInt(radius.toString());
+      edge = edge.toString().trim();
 
-        // Draw cutouts
-        ctx.globalCompositeOperation = 'destination-out';
+      // Draw cutouts
+      ctx.globalCompositeOperation = "destination-out";
 
-        if (cutoutTop) {
-            drawCutout(0, 0);
-            drawCutout(width, 0);
-        }
-        if (cutoutBottom) {
-            drawCutout(width, height);
-            drawCutout(0, height);
-        }
+      if (edge === "top" || edge === "both") {
+        drawCutout(0, 0);
+        drawCutout(width, 0);
+      }
+      if (edge === "bottom" || edge === "both") {
+        drawCutout(width, height);
+        drawCutout(0, height);
+      }
     }
-});
+  }
+);
